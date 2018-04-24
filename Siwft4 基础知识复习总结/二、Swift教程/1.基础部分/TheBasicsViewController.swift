a4bec5c62b21101e54c9f7a2720f5a8e24835200
KDtrey35 <823394434@qq.com>
2018-04-24 15:51:32 +0800

    Update Swift4
    update Swift4

---
+++ Siwft4 基础知识复习总结/二、Swift教程/1.基础部分/TheBasicsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TheBasicsViewController.swift
+//  Siwft4 基础知识复习总结
+//
+//  Created by JDK-D02 on 2018/4/24.
+//  Copyright © 2018年 JDK-002. All rights reserved.
+//
+
+import UIKit
+
+class TheBasicsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
